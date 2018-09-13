784bb1625be22bdf970504c5f9d026b3f590a85d
José Sánchez-Gallego <gallegoj@uw.edu>
2018-09-12 19:07:58 -0700

    Stub for GetID

--- python/jaeger/commands/status.py
+++ python/jaeger/commands/status.py
@@ -7,4 +7,11 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2018-08-29 11:46:38
+# @Last modified time: 2018-09-12 09:15:28
+
+
+from .commands import Command
+
+
+class GetID(Command):
+    pass
