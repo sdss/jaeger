e99c8a119b24ac313abc48e1407cb83dec8bd3eb
José Sánchez-Gallego <gallegoj@uw.edu>
2018-09-12 20:44:33 -0700

    Implement _recv_internal for BusTester

--- python/jaeger/tests/bus.py
+++ python/jaeger/tests/bus.py
@@ -7,24 +7,46 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2018-09-06 17:36:33
+# @Last modified time: 2018-09-12 20:44:07
 
 
+import asyncio
 import os
 import pty
 import re
 
 import serial
+
+import can
 from can.bus import BusABC
 
 
 class BusTester(BusABC):
 
-    def __init__(self):
+    def __init__(self, fps=None):
 
-        self._reply_queue = []
+        self.fps = fps
+        self._reply_queue = asyncio.Queue()
 
         super().__init__('')
+
+    def _recv_internal(self, timeout=None):
+        """Retrieves and returns a message from the internal reply queue."""
+
+        async def coro(timeout):
+            try:
+                reply = await asyncio.wait_for(self._reply_queue.get(), timeout, loop=self.loop)
+                assert isinstance(reply, can.Message)
+                return reply, False
+            except asyncio.TimeoutError:
+                return None, False
+
+        return self.loop.run_until_complete(coro(timeout))
+
+    def send(self, message):
+        """Receives and processes a message."""
+
+        pass
 
 
 class SlcanBusTester(BusTester):
