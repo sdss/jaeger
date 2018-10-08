d25ec932870af483020cd416e7d2e3adbf63cc85
José Sánchez-Gallego <gallegoj@uw.edu>
2018-10-07 23:34:42 -0700

    Add GetActualPostion command

--- python/jaeger/commands/status.py
+++ python/jaeger/commands/status.py
@@ -7,10 +7,13 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2018-10-07 21:34:05
+# @Last modified time: 2018-10-07 23:33:55
 
 
-from jaeger.commands import Command, CommandID
+import numpy
+
+from jaeger.commands import MOTOR_STEPS, Command, CommandID
+from jaeger.utils import bytes_to_int
 
 
 __ALL__ = ['GetID', 'GetStatus']
@@ -32,3 +35,29 @@ class GetStatus(Command):
 
     command_id = CommandID.GET_STATUS
     broadcastable = True
+
+
+class GetActualPosition(Command):
+
+    command_id = CommandID.GET_ACTUAL_POSTION
+    broadcastable = False
+
+    def get_postions(self):
+        """Returns the positions of alpha and beta in degrees.
+
+        Raises
+        ------
+        ValueError
+            If no reply has been received or the data cannot be parsed.
+
+        """
+
+        if len(self.replies) == 0:
+            raise ValueError('no positioners have replied to this command.')
+
+        data = self.replies[0].data
+
+        alpha = bytes_to_int(data[0:4])
+        beta = bytes_to_int(data[4:])
+
+        return numpy.array([alpha, beta]) / MOTOR_STEPS * 360.
