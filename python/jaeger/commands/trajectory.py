3e1436271f0f6b7095e6290d97f26c0c6ad1aa2a
José Sánchez-Gallego <gallegoj@uw.edu>
2018-10-08 23:24:04 -0700

    Make SendTrajectoryData output multiple messages

--- python/jaeger/commands/trajectory.py
+++ python/jaeger/commands/trajectory.py
@@ -7,10 +7,12 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2018-10-08 18:30:00
+# @Last modified time: 2018-10-08 22:51:04
 
+import numpy
 
 from jaeger.commands import MOTOR_STEPS, TIME_STEP, Command, CommandID
+from jaeger.commands.base import Message
 from jaeger.utils import int_to_bytes
 
 
@@ -38,20 +40,55 @@ class SendNewTrajectory(Command):
 
 
 class SendTrajectoryData(Command):
-    """Sends a data point in the trajectory."""
+    """Sends a data point in the trajectory.
+
+    This command sends multiple messages that represent a full trajectory.
+
+    Parameters
+    ----------
+    alpha : `list`
+        For the alpha arm, a list of tuples in which the first element is the
+        angle, in degrees, and the second is the associated time, in seconds.
+    beta : `list`
+        As ``alpha``, for the beta arm.
+
+    Examples
+    --------
+        >>> SendTrajectoryData(alpha=[(90, 10), (88, 12), (80, 20)],
+                               beta=[(10, 5), (30, 35)])
+
+    """
 
     command_id = CommandID.SEND_TRAJECTORY_DATA
     broadcastable = False
 
-    def __init__(self, angle, time, **kwargs):
+    def __init__(self, alpha, beta, **kwargs):
 
-        angle_steps = int(angle / 360. * MOTOR_STEPS)
-        time_steps = int(time / TIME_STEP)
+        alpha = numpy.array(alpha)
+        beta = numpy.array(beta)
 
-        data = int_to_bytes(time_steps) + int_to_bytes(angle_steps)
-        kwargs['data'] = data
+        alpha[:, 0] = alpha[:, 0] / 360. * MOTOR_STEPS
+        beta[:, 0] = beta[:, 0] / 360. * MOTOR_STEPS
+        alpha[:, 1] /= TIME_STEP
+        beta[:, 1] /= TIME_STEP
+
+        self.alpha_points = alpha.astype(numpy.int)
+        self.beta_points = beta.astype(numpy.int)
 
         super().__init__(**kwargs)
+
+    def get_messages(self):
+        """Returns the list of messages associated with this command."""
+
+        messages = []
+
+        for angle, time in numpy.vstack((self.alpha_points, self.beta_points)):
+
+            data = int_to_bytes(time) + int_to_bytes(angle)
+            messages.add(
+                Message(self, positioner_id=self.positioner_id, data=data))
+
+        return messages
 
 
 class TrajectoryDataEnd(Command):
