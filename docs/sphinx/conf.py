63e0cc86199636145a4e1c95a3860fbdb9811ff5
José Sánchez-Gallego <gallegoj@uw.edu>
2018-10-10 09:19:32 -0700

    Autodoc groupwise

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -7,7 +7,7 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2018-09-18 14:19:53
+# @Last modified time: 2018-10-10 08:46:14
 
 
 # import matplotlib
@@ -112,7 +112,6 @@ autodoc_default_options = {
 
 napoleon_use_rtype = False
 napoleon_use_ivar = False
-autodoc_member_order = 'bysource'
 
 rst_epilog = """
 """
