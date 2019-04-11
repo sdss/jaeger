d5cab1aa69d77b14f727eaf78349ab27b3026ceb
José Sánchez-Gallego <gallegoj@uw.edu>
2019-04-11 14:24:04 +0200

    Revert "Fix typo"
    This reverts commit 0b8e5a2476a1ef566dc04673173c8862bed31995.

--- python/jaeger/commands/bootloader.py
+++ python/jaeger/commands/bootloader.py
@@ -7,7 +7,7 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2019-04-11 11:38:23
+# @Last modified time: 2019-04-11 10:56:37
 
 import asyncio
 import os
@@ -219,7 +219,7 @@ class SendFirmwareData(commands.Command):
         """
 
         messages = [commands.Message(self, positioner_id=self.positioner_id, data=data_chunk)
-                    for data_chunk in self._data]
+                    for data_chunk in self.data]
 
         self._n_messages = len(messages)
 
