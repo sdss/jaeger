960869b5e2a7b98203d2b8caa90231cda6082b47
José Sánchez-Gallego <gallegoj@uw.edu>
2019-05-13 18:03:28 -0700

    Await goto command

--- python/jaeger/actor/commands.py
+++ python/jaeger/actor/commands.py
@@ -7,7 +7,7 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2019-05-13 17:29:57
+# @Last modified time: 2019-05-13 17:56:58
 
 import click
 
@@ -19,9 +19,11 @@ from clu import command_parser as jaeger_parser
 @click.argument('alpha', type=click.FloatRange(0., 360.))
 @click.argument('beta', type=click.FloatRange(0., 360.))
 @click.option('--speed', type=click.FloatRange(0., 2000.), nargs=2)
-async def goto(command, fps, positioner_id, alpha, beta, speed=[None, None]):
+async def goto(command, fps, positioner_id, alpha, beta, speed=None):
     """Sends a positioner to a given (alpha, beta) position."""
 
-    fps.positioners[positioner_id].goto(alpha, beta,
-                                        alpha_speed=speed[0],
-                                        beta_speed=speed[1])
+    speed = speed or [None, None]
+
+    await fps.positioners[positioner_id].goto(alpha, beta,
+                                              alpha_speed=speed[0],
+                                              beta_speed=speed[1])
