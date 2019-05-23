f7b8453ecfced4cfd530a4253a68bbd43e822dfc
José Sánchez-Gallego <gallegoj@uw.edu>
2019-05-22 19:15:01 -0700

    Change goto and initialise to use as_complete_failer

--- python/jaeger/actor/commands.py
+++ python/jaeger/actor/commands.py
@@ -7,15 +7,17 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2019-05-13 17:56:58
+# @Last modified time: 2019-05-22 19:14:44
 
 import click
 
+import clu
 from clu import command_parser as jaeger_parser
+from jaeger.utils import as_complete_failer
 
 
 @jaeger_parser.command()
-@click.argument('positioner-id', type=int)
+@click.argument('POSITIONER-ID', type=int, nargs=-1)
 @click.argument('alpha', type=click.FloatRange(0., 360.))
 @click.argument('beta', type=click.FloatRange(0., 360.))
 @click.option('--speed', type=click.FloatRange(0., 2000.), nargs=2)
@@ -24,6 +26,33 @@ async def goto(command, fps, positioner_id, alpha, beta, speed=None):
 
     speed = speed or [None, None]
 
-    await fps.positioners[positioner_id].goto(alpha, beta,
-                                              alpha_speed=speed[0],
-                                              beta_speed=speed[1])
+    tasks = []
+    for pid in positioner_id:
+        tasks.append(fps.positioners[pid].goto(alpha, beta,
+                                               alpha_speed=speed[0],
+                                               beta_speed=speed[1]))
+
+    result = await as_complete_failer(tasks, on_fail_callback=fps.abort_trajectory)
+
+    if not result:
+        command.set_status(clu.CommandStatus.FAILED, text='goto failed')
+    else:
+        command.set_status(clu.CommandStatus.DONE, text='Position reached')
+
+
+@jaeger_parser.command()
+@click.argument('positioner-id', type=int, nargs=-1)
+@click.option('--datums', is_flag=True, help='If set, initialises the datums.')
+async def initialise(command, fps, positioner_id, datums=False):
+    """Sends a positioner to a given (alpha, beta) position."""
+
+    tasks = []
+    for pid in positioner_id:
+        tasks.append(fps.positioners[pid].initialise(initialise_datums=datums))
+
+    result = await as_complete_failer(tasks)
+
+    if not result:
+        command.set_status(clu.CommandStatus.FAILED, text='initialise failed')
+    else:
+        command.set_status(clu.CommandStatus.DONE, text='Initialisation complete')
