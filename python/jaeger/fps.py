ecc68c04a75eec3a74f7ec9757a9f05a3390d2db
José Sánchez-Gallego <gallegoj@uw.edu>
2019-08-30 13:25:38 -0400

    Get positioner id from file

--- python/jaeger/fps.py
+++ python/jaeger/fps.py
@@ -7,7 +7,7 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2019-06-18 16:57:36
+# @Last modified time: 2019-08-30 13:25:24
 
 import asyncio
 import os
@@ -93,13 +93,11 @@ class BaseFPS(object):
             data = astropy.table.Table.read(layout, format='ascii.no_header',
                                             names=['row', 'pos', 'x', 'y', 'type'])
 
-            pos_id = 1
             for row in data:
                 if row['type'].lower() == 'fiducial':
                     continue
                 new_positioner = self._positioner_class(
-                    pos_id, self, centre=(row['x'], row['y']))
-                pos_id += 1
+                    row['pos'], self, centre=(row['x'], row['y']))
                 self.add_positioner(new_positioner)
 
             n_pos = pos_id - 1
