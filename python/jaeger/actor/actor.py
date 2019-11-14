1db68ac2fc792d496472493715fe071e0fb9d776
José Sánchez-Gallego <gallegoj@uw.edu>
2019-11-14 14:49:25 +0100

    Better handle disconnected WAGO

--- python/jaeger/actor/actor.py
+++ python/jaeger/actor/actor.py
@@ -37,7 +37,7 @@ class JaegerActor(clu.LegacyActor):
         log.addHandler(self.actor_handler)
         self.actor_handler.setLevel(logging.ERROR)
 
-        if fps.wago:
+        if fps.wago and fps.wago.connected:
             self.timer_commands.add_command('wago status', delay=10)
 
     @classmethod
