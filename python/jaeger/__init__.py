bd74f9e55587251df2d55f4046ddccd7c1376263
José Sánchez-Gallego <gallegoj@uw.edu>
2019-11-21 20:36:52 +0100

    Set __version__ from distribution or pyproject

--- python/jaeger/__init__.py
+++ python/jaeger/__init__.py
@@ -8,10 +8,23 @@ import warnings
 
 from .core import get_config, get_logger
 
+import pkg_resources
+
+
+try:
+    __version__ = pkg_resources.get_distribution('jaeger').version
+except pkg_resources.DistributionNotFound:
+    try:
+        import toml
+        poetry_config = toml.load(open(os.path.join(os.path.dirname(__file__),
+                                                    '../../pyproject.toml')))
+        __version__ = poetry_config['tool']['poetry']['version']
+    except Exception:
+        warnings.warn('cannot find jaeger version. Using 0.0.0.', UserWarning)
+        __version__ = '0.0.0'
+
 
 NAME = 'jaeger'
-
-__version__ = '0.4.1-dev'
 
 
 config = get_config(NAME, allow_user=True)
