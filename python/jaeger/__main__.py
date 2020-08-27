a21219c4e21307315609e81c497730c9b313dd77
José Sánchez-Gallego <gallegoj@uw.edu>
2020-08-27 12:47:53 -0700

    Use close and open for relays in upgrade-firmware

--- python/jaeger/__main__.py
+++ python/jaeger/__main__.py
@@ -175,9 +175,9 @@ async def upgrade_firmware(fps_maker, firmware_file, force, positioners, cycle):
 
         if fps.ieb and cycle:
             log.info('power cycling positioners')
-            await fps.ieb.turn_off('24V')
+            await fps.ieb.open('24V')
             await asyncio.sleep(5)
-            await fps.ieb.turn_on('24V')
+            await fps.ieb.close('24V')
             await asyncio.sleep(3)
             await fps.initialise()
 
