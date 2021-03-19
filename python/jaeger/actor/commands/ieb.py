5e0da0a6b26407a3987246d13f6b9dd067498e98
José Sánchez-Gallego <gallegoj@uw.edu>
2021-03-19 14:19:29 -0700

    Add power on/off sequence

--- python/jaeger/actor/commands/ieb.py
+++ python/jaeger/actor/commands/ieb.py
@@ -12,6 +12,8 @@ import click
 
 from clu.parser import pass_args
 
+from jaeger.fps import IEB
+
 from . import jaeger_parser
 
 
@@ -61,7 +63,7 @@ async def status(command, fps):
             elif meas == "open":
                 meas = "off"
             value_unit = f"{meas}" if not units else f"{meas} {units}"
-            measured.append(f"{dev_name}={value_unit}")
+            measured.append(f"{dev_name.lower()}={value_unit}")
         command.write("i", message="; ".join(measured))
 
     return command.finish()
@@ -126,3 +128,155 @@ async def switch(command, fps, device, on, cycle):
     status = "on" if (await device_obj.read())[0] == "closed" else "off"
 
     return command.finish(text=f"device {dev_name!r} is now {status!r}.")
+
+
+async def _power_sequence(command, ieb, seq, mode="on") -> bool:
+    """Applies the power on/off sequence."""
+
+    relay_result = "closed" if mode == "on" else "open"
+
+    command.info(text=f"Running power {mode} sequence")
+
+    # First check that the SYNC line is open.
+    sync = ieb.get_device("DO.SYNC")
+    if (await sync.read())[0] == "closed":
+        command.debug(text="SYNC line is high. Opening it.")
+        await sync.open()  # type: ignore
+
+    if not (await sync.read())[0] == "open":
+        command.fail(error="Failed opening SYNC line.")
+        return False
+
+    command.debug(sync="off")
+
+    for devname in seq:
+        if isinstance(devname, str):
+            dev = ieb.get_device(f"DO.{devname}")
+
+            if (await dev.read())[0] == relay_result:
+                command.debug(text=f"{devname} alredy powered {mode}.")
+            else:
+                command.debug(text=f"Powering {mode} {devname}.")
+                await dev.close() if mode == "on" else dev.open()  # type: ignore
+                if (await dev.read())[0] != relay_result:
+                    command.fail(error=f"Failed powering {mode} {devname}.")
+                    return False
+
+            command.debug({devname.lower(): "on"})
+
+        elif isinstance(devname, (tuple, list)):
+            devname = list(devname)
+            devs = [ieb.get_device(f"DO.{dn}") for dn in devname]
+
+            status = list(await asyncio.gather(*[dev.read() for dev in devs]))
+
+            for ii, res in enumerate(status.copy()):
+                if res[0] == relay_result:
+                    command.debug(
+                        {
+                            "text": f"{devname[ii]} alredy powered {mode}.",
+                            devname[ii].lower(): relay_result,
+                        }
+                    )
+                    devs.pop(ii)
+                    devname.pop(ii)
+
+            command.debug(text=f"Powering {mode} {', '.join(devname)}")
+
+            await asyncio.gather(*[dev.close() for dev in devs])  # type: ignore
+
+            status = list(await asyncio.gather(*[dev.read() for dev in devs]))
+            for ii, res in enumerate(status):
+                if res[0] != "closed":
+                    command.fail(error=f"Failed powering {mode} {devname[ii]}.")
+                    return False
+
+        else:
+            command.fail(error=f"Invalid relay {devname!r}.")
+            return False
+
+    return True
+
+
+@ieb.group()
+def power(command, fps):
+    """Runs the power on/off sequences."""
+
+    pass
+
+
+@power.command()
+async def on(command, fps):
+    """Powers on all the FPS IEB components."""
+
+    ieb: IEB = fps.ieb
+
+    # Sequence of relays to power on. Tuples indicate relays that can be powered
+    # on concurrently.
+    on_seq = [
+        ("CM1", "CM2", "CM3", "CM4", "CM5", "CM6"),
+        "PS1",
+        "PS2",
+        "PS3",
+        "PS4",
+        "PS5",
+        "PS6",
+        "GFA1",
+        "GFA2",
+        "GFA3",
+        "GFA4",
+        "GFA5",
+        "GFA6",
+        "NUC1",
+        "NUC2",
+        "NUC3",
+        "NUC4",
+        "NUC5",
+        "NUC6",
+    ]
+
+    if not (await _power_sequence(command, ieb, on_seq, mode="on")):
+        return
+
+    return command.finish(text="Power on sequence complete.")
+
+
+@power.command()
+@click.option("--nucs", is_flag=True, help="Also power down NUCs")
+async def off(command, fps, nucs):
+    """Powers off all the FPS IEB components."""
+
+    ieb: IEB = fps.ieb
+
+    # Sequence of relays to power off. Tuples indicate relays that can be powered
+    # off concurrently.
+    off_seq = [
+        ("CM1", "CM2", "CM3", "CM4", "CM5", "CM6"),
+        "PS1",
+        "PS2",
+        "PS3",
+        "PS4",
+        "PS5",
+        "PS6",
+        "GFA1",
+        "GFA2",
+        "GFA3",
+        "GFA4",
+        "GFA5",
+        "GFA6",
+    ]
+
+    if nucs:
+        off_seq += [
+            "NUC1",
+            "NUC2",
+            "NUC3",
+            "NUC4",
+            "NUC5",
+            "NUC6",
+        ]
+
+    if not (await _power_sequence(command, ieb, off_seq, mode="off")):
+        return
+
+    return command.finish(text="Power off sequence complete.")
