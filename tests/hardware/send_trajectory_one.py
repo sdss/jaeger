47463a4a5c03ea40e17d48b8e064cbe4f24fa255
José Sánchez-Gallego <gallegoj@uw.edu>
2021-07-18 18:09:48 -0700

    Functional test to send one trajectory to positioner 20

---
+++ tests/hardware/send_trajectory_one.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2021-07-18
+# @Filename: send_trajectory_one.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+import asyncio
+
+from jaeger.fps import FPS
+
+
+async def main():
+
+    fps = await FPS.create()
+
+    assert 20 in fps and len(fps) == 1, "this test requires only positioner 20."
+
+    print("Going to start position.")
+    await fps[20].goto(90, 180)
+
+    print("Sending trajectory.")
+    await fps.send_trajectory(
+        {20: {"alpha": [(90, 0), (0, 20)], "beta": [(180, 0), (170, 15)]}},
+        use_sync_line=False,
+    )
+
+
+asyncio.run(main())
