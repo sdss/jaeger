d375068b06a3d73ca75e56da0bd3179863ba026d
José Sánchez-Gallego <gallegoj@uw.edu>
2021-08-25 04:36:57 +0000

    Additional FVC commands

--- python/jaeger/actor/commands/fvc.py
+++ python/jaeger/actor/commands/fvc.py
@@ -6,8 +6,14 @@
 # @Filename: fvc.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
+from __future__ import annotations
+
+from typing import cast
+
+import click
+
 from clu import Command
-from drift import DriftError
+from drift import DriftError, Relay
 
 from jaeger import FPS
 from jaeger.ieb import FVC
@@ -46,9 +52,73 @@ async def status(command: Command[JaegerActor], fps: FPS):
                     value = False
                 else:
                     value = round(value, 1)
-                status["fvc_" + category].append(value)
+                status[category].append(value)
 
         command.finish(status)
 
     except DriftError:
         return command.fail(error="FVC IEB is unavailable or failed to connect.")
+
+
+async def _power_device(device: str, mode: str):
+    """Power on/off the device."""
+
+    fvc_ieb = FVC.create()
+
+    dev: Relay = cast(Relay, fvc_ieb.get_device(device))
+    if mode == "on":
+        await dev.close()
+    else:
+        await dev.open()
+
+
+async def _execute_on_off_command(
+    command: Command[JaegerActor], device: str, mode: str
+):
+    """Executes the on/off command."""
+
+    mode = mode.lower()
+
+    try:
+        await _power_device(device, mode)
+        command.info(text=f"{device} is now {mode}.")
+    except DriftError:
+        return command.fail(error=f"Failed to turn {device} {mode}.")
+
+    status_cmd = Command("fvc status", parent=command)
+    await status_cmd.parse()
+
+    return command.finish()
+
+
+@fvc.command()
+@click.argument("MODE", type=click.Choice(["on", "off"], case_sensitive=False))
+async def camera(command: Command[JaegerActor], fps: FPS, mode: str):
+    """Turns camera on/off."""
+
+    await _execute_on_off_command(command, "FVC", mode)
+
+
+@fvc.command()
+@click.argument("MODE", type=click.Choice(["on", "off"], case_sensitive=False))
+async def NUC(command: Command[JaegerActor], fps: FPS, mode: str):
+    """Turns NUC on/off."""
+
+    await _execute_on_off_command(command, "NUC", mode)
+
+
+@fvc.command()
+@click.argument("LEVEL", type=int)
+async def led(command: Command[JaegerActor], fps: FPS, level: int):
+    """Sets the level of the FVC LED."""
+
+    fvc_ieb = FVC.create()
+    led = fvc_ieb.get_device("LED1")
+
+    raw_value = 32 * int(1023 * (level / 100))
+    await led.write(raw_value)
+
+    status_cmd = Command("fvc status", parent=command)
+    await status_cmd.parse()
+
+    return command.finish()
