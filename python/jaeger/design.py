8a666eb44b7b2e8a4f5a4c27a9ae0daaa43f2d6c
José Sánchez-Gallego <gallegoj@uw.edu>
2021-10-16 13:40:36 -0700

    Complete transformation from ICRS to Positioner coords

--- python/jaeger/design.py
+++ python/jaeger/design.py
@@ -8,16 +8,23 @@
 
 from __future__ import annotations
 
-from typing import Optional
+import warnings
+
+from typing import Optional, Union, cast
 
 import numpy
+import pandas
 import peewee
-from coordio import ICRS, Field, FocalPlane, Observed, Site, Tangent, Wok
+from coordio import ICRS, Field, FocalPlane, Observed, Site, Tangent, Wok, positioner
 from coordio.defaults import INST_TO_WAVE, positionerTable, wokCoords
 
 from sdssdb.peewee.sdss5db import targetdb
 
 from jaeger import log
+from jaeger.exceptions import JaegerError, JaegerUserWarning
+
+
+PositionerType = Union[positioner.PositionerApogee, positioner.PositionerBoss]
 
 
 class Design:
@@ -56,6 +63,9 @@ class PositionerGrid:
     observed: Observed
     focal: FocalPlane
     tangent: Tangent
+    positioner_objs: list[PositionerType]
+    valid: numpy.ndarray
+    positioner: numpy.ndarray
 
     def __init__(self, design: Design, assignments: list[targetdb.Assignment]):
 
@@ -69,28 +79,43 @@ class PositionerGrid:
         wok_table = wokCoords[wokCoords.wokType == self.observatory]
         wok_table.set_index("holeID", inplace=True)
 
-        self.assignments = assignments
-        self.holeids = [assignment.hole.holeid for assignment in assignments]
+        self.assignments = [
+            assg
+            for assg in assignments
+            if assg.instrument.label.lower()  # TODO: remove this when RS matches wok.
+            in wok_table.loc[assg.hole.holeid].holeType.lower()
+        ]
 
-        self.positioner_ids: list[int]
+        self.holeids: list[str] = [assg.hole.holeid for assg in self.assignments]
+
         self.positioner_ids = positioner_table.loc[self.holeids].positionerID.tolist()
+        self.positioner_ids = cast(list[int], self.positioner_ids)
 
         self.targets: list[targetdb.Target] = [
             assignment.carton_to_target.target for assignment in self.assignments
         ]
 
         self.wok_data = wok_table.loc[self.holeids]
+        assert isinstance(self.wok_data, pandas.DataFrame)
+
         assert len(self.wok_data) == len(self.holeids), "invalid number of hole_ids"
 
-        self.fibre_types = [assg.instrument.label for assg in self.assignments]
-        self.wavelengths = [INST_TO_WAVE[ft.capitalize()] for ft in self.fibre_types]
+        self.fibre_types: list[str] = [
+            assg.instrument.label for assg in self.assignments
+        ]
+        self.wavelengths: list[float] = [
+            INST_TO_WAVE[ft.capitalize()] for ft in self.fibre_types
+        ]
 
-        # if (
-        #     not self.wok_data.reset_index()
-        #     .apply(lambda row: self.fibre_types[row.name] in row.holeType, axis=1)
-        #     .all()
-        # ):
-        #     raise RuntimeError("Mismatch of fibre types to positioners.")
+        if (
+            not self.wok_data.reset_index()
+            .apply(
+                lambda row: self.fibre_types[row.name].lower() in row.holeType.lower(),
+                axis=1,
+            )
+            .all()
+        ):
+            raise RuntimeError("Mismatch of fibre types to positioners.")
 
         self.compute_coordinates()
 
@@ -108,7 +133,12 @@ class PositionerGrid:
             .join(targetdb.CartonToTarget)
             .join(targetdb.Assignment)
             .join(targetdb.Design)
-            .where(targetdb.Design.design_id == self.design_id)
+            .switch(targetdb.Assignment)
+            .join(targetdb.Hole)
+            .where(
+                targetdb.Design.design_id == self.design_id,
+                targetdb.Hole.holeid.in_(self.holeids),
+            )
             .tuples()
         )
         target_data = numpy.array(target_data, dtype=numpy.float64)
@@ -123,8 +153,9 @@ class PositionerGrid:
         )
 
         self.site.set_time(jd)
+
         self.observed = Observed(
-            self.icrs,
+            [[1, 2, 3]],
             wavelength=self.wavelengths,
             site=self.site,
         )
@@ -140,6 +171,7 @@ class PositionerGrid:
             self.observed,
             field_center=self.observed_boresight,
         )
+
         self.focal = FocalPlane(
             field,
             wavelength=self.wavelengths,
@@ -150,5 +182,54 @@ class PositionerGrid:
             site=self.site,
             obsAngle=self.design.field.position_angle,
         )
+        self.tangent = Tangent(
+            wok,
+            holeID=self.holeids,
+            site=self.site,
+            wavelength=self.wavelengths,  # This just to prevent a warning.
+        )
+
+        # coordio doesn't allow a single instance of PositionerBase to contain
+        # an array of holeIDs and fibre types. For now we create a list of positioner
+        # instances for each hole and fibre.
+        self.positioner_objs = []
+        for ipos in range(len(self.targets)):
+            tan_coords = self.tangent[[ipos]]
+
+            ftype = self.fibre_types[ipos]
+            holeid = self.holeids[ipos]
+
+            if ftype.upper() == "BOSS":
+                positioner_class = positioner.PositionerBoss
+            elif ftype.upper() == "APOGEE":
+                positioner_class = positioner.PositionerApogee
+            else:
+                raise ValueError(f"Invalid fibre type {ftype}.")
+
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                self.positioner_objs.append(
+                    positioner_class(
+                        tan_coords,
+                        site=self.site,
+                        holeID=holeid,
+                    )
+                )
+
+        if any([pos.positioner_warn[0] for pos in self.positioner_objs]):
+            warnings.warn(
+                "Some coordinates failed while converting to "
+                "positioner coordinates. Skipping.",
+                JaegerUserWarning,
+            )
+
+        self.valid = numpy.where([~p.positioner_warn[0] for p in self.positioner_objs])
+
+        self.positioner = numpy.vstack(
+            [p.astype(numpy.float32) for p in self.positioner_objs]
+        )
+
+        if not (self.positioner[self.valid][:, 1] < 180).all():
+            raise JaegerError("Some beta coordinates are < 180.")
 
         self.tangent = Tangent(wok, holeID=self.holeids, site=self.site)
