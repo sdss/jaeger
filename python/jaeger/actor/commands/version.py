07257e64d32f18994df35fa6b1dc21c032ee7762
José Sánchez-Gallego <gallegoj@uw.edu>
2022-01-14 15:18:07 -0800

    Move fps_calibrations import inside try-except

--- python/jaeger/actor/commands/version.py
+++ python/jaeger/actor/commands/version.py
@@ -9,7 +9,6 @@
 from __future__ import annotations
 
 from coordio import __version__ as coordio_version
-from fps_calibrations import get_version
 from kaiju import __version__ as kaiju_version
 
 from jaeger import __version__ as jaeger_version
@@ -27,6 +26,12 @@ async def version(command, fps):
     command.info(version=jaeger_version)
     command.info(kaiju_version=kaiju_version)
     command.info(coordio_version=coordio_version)
-    command.info(fps_calibrations_version=get_version())
+
+    try:
+        from fps_calibrations import get_version
+
+        command.info(fps_calibrations_version=get_version())
+    except ImportError:
+        command.warning("Cannot output fps_calibrations version.")
 
     return command.finish()
