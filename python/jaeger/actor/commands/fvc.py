9aef256f5695a07643224529eeff51ffff36ae08
José Sánchez-Gallego <gallegoj@uw.edu>
2023-07-10 21:30:35 +0000

    Typing

--- python/jaeger/actor/commands/fvc.py
+++ python/jaeger/actor/commands/fvc.py
@@ -338,7 +338,7 @@ async def take_fvc_loop(
     use_invkin: bool = True,
     no_write_summary: bool = False,
     configuration: BaseConfiguration | None = None,
-    polids: str | None = None,
+    polids: list[int] | str | None = None,
 ):
     """Helper to take an FVC loop that can be called externally."""
 
