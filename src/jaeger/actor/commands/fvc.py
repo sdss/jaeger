20cdb8087c474c8735be0ff56e3c51cff2b7a206
José Sánchez-Gallego <gallegoj@uw.edu>
2024-08-09 19:02:34 +0000

    Fix passing rotator_reference_angle argument

--- src/jaeger/actor/commands/fvc.py
+++ src/jaeger/actor/commands/fvc.py
@@ -148,7 +148,7 @@ async def expose(
     help="Comma-separated ZB orders to use for the FVC transformation.",
 )
 @click.option(
-    "--rotator-reference-position",
+    "--rotator-reference-angle",
     type=float,
     help="Rotator reference position for fiducial fitting.",
 )
@@ -367,7 +367,7 @@ async def take_fvc_loop(
     no_write_summary: bool = False,
     configuration: BaseConfiguration | None = None,
     polids: list[int] | str | None = None,
-    rot_ref_angle: float | None = None,
+    rotator_reference_angle: float | None = None,
 ):
     """Helper to take an FVC loop that can be called externally."""
 
@@ -451,7 +451,7 @@ async def take_fvc_loop(
                 polids=polids,
                 centroid_method=centroid_method,
                 use_new_invkin=use_invkin,
-                rot_ref_angle=rot_ref_angle,
+                rot_ref_angle=rotator_reference_angle,
                 # loop=asyncio.get_running_loop(),  # Disable for now
             )
 
